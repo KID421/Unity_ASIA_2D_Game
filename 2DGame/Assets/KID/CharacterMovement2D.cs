cbeb354f1ca8ec3e4cac41f16ef347493d1db3a5
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-03 15:57:47 +0800

    碰撞偵測

--- 2DGame/Assets/KID/CharacterMovement2D.cs
+++ 2DGame/Assets/KID/CharacterMovement2D.cs
@@ -2,5 +2,31 @@
 
 public class CharacterMovement2D : MonoBehaviour
 {
+    [Header("移動速度"), Range(0, 10)]
+    public float speed = 1;
 
+    private Rigidbody2D rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (Input.anyKey)
+        {
+            rig.MovePosition(transform.position + Vector3.right * h * Time.fixedDeltaTime * speed);
+
+            if (h == 1) transform.eulerAngles = Vector3.zero;
+            else if (h == -1) transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
 }
