d8889cdf671490771f08d249f15403422cca283d
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-10 15:57:22 +0800

    添加攝影機追蹤

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;   // 引用 介面 API
 
 public class Player : MonoBehaviour
 {
@@ -7,6 +8,10 @@ public class Player : MonoBehaviour
     // 修飾詞 類型 名稱 (指定 值)；
     // None 空值：沒東西 - 程式錯誤
     public GameObject final;
+    // 道具數量
+    public Text textCount;
+    // 數量
+    public int count;
 
     // 碰撞偵測條件
     // 1. 兩個物件必須有碰撞器 Collider2D
@@ -20,6 +25,18 @@ public class Player : MonoBehaviour
         if (collision.name == "傳送門")
         {
             final.SetActive(true);  // 結束畫面.啟動設定(是) - 顯示
+        }
+
+        // 如果碰到物件的標籤式櫻桃就吃掉她
+        if (collision.tag == "櫻桃")
+        {
+            // 刪除(碰到的遊戲物件)
+            // gameObject 此腳本的遊戲物件
+            Destroy(collision.gameObject);
+            // 遞增
+            count++;
+            // 道具數量的文字 = "櫻桃數量：" + 數量
+            textCount.text = "櫻桃數量：" + count;
         }
     }
 }
