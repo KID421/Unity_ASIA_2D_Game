67509f5fcf3d1a3222af22b6d8732d4a4730fb6e
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-10 15:13:44 +0800

    攝影機

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -11,9 +11,15 @@ public class Player : MonoBehaviour
     // 碰撞偵測條件
     // 1. 兩個物件必須有碰撞器 Collider2D
     // 2. 兩者必須有至少一個剛體 Rigidbody2D
+    // collision 儲存碰到物件的資訊
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送們惹~");
-        final.SetActive(true);  // 結束畫面.啟動設定(是) - 顯示
+        // 判斷式 if 語法：
+        // if(條件) { 條件成立時會執行的程式區塊 }
+        // 如果碰到物件的名稱是傳送門就...
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);  // 結束畫面.啟動設定(是) - 顯示
+        }
     }
 }
